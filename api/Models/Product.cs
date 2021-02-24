1afe1421821f66be9d62b1ac2190c6a170bf1c63
Hugo Flausino <hugo.flausino@hotmail.com>
2021-02-24 06:22:28 -0300

    Created model Product

---
+++ api/Models/Product.cs
@@ -0,0 +1,16 @@
+namespace api.Models
+{
+    public class Product
+    {
+        public Product(int id, string description, decimal price)
+        {
+            Id = id;
+            Description = description;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
