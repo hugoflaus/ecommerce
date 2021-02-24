32ca064852a00ef500ec308641294d145afa2d6c
Hugo Flausino <hugo.flausino@hotmail.com>
2021-02-24 07:31:35 -0300

    completed crud controller products

--- api/Controllers/ProductsController.cs
+++ api/Controllers/ProductsController.cs
@@ -41,6 +41,12 @@ namespace api.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] ProductInputModel productInputModel)
         {
+
+            if(productInputModel == null)
+            {
+                return BadRequest();
+            }
+
             var product = new Product(productInputModel.Description, productInputModel.Price);
 
             _ecommerceDBContext.Produtcs.Add(product);
@@ -48,6 +54,46 @@ namespace api.Controllers
 
             return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
 
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] ProductInputModel productInputModel, int id)
+        {
+            if(productInputModel == null)
+            {
+                return BadRequest();
+            }
+
+            var product = _ecommerceDBContext.Produtcs.SingleOrDefault(p => p.Id == id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            product.Description = productInputModel.Description;
+            product.Price = productInputModel.Price;
+
+            _ecommerceDBContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product  = _ecommerceDBContext.Produtcs.SingleOrDefault(p => p.Id == id);
+            
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            _ecommerceDBContext.Produtcs.Remove(product);
+
+            _ecommerceDBContext.SaveChanges();
+
+            return NoContent();
         }
 
     }
